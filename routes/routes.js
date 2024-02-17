37c939a95c5a682b8b6bc49c78841bcdfcc18e41
Vikash Rana <vikasrana1998@gmail.com>
2024-02-17 18:50:51 +0530

    naming convention fixes

--- routes/routes.js
+++ routes/routes.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const adminRoutes = require('./admin');
-const userRoutes = require('./users');
+const publicRoutes = require('./public');
 const postController = require('../controllers/postController');
 
 // router.use('/admin', adminRoutes);
-// router.use('/user', userRoutes);
+// router.use('/public', publicRoutes);
 router.get('/try', postController.index);
 
 module.exports = router;
