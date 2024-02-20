036e7ca02a610e8ca55332ad7fd318d955236226
Vikash Rana <vikasrana1998@gmail.com>
2024-02-20 21:34:56 +0530

    stale code removed

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 require('dotenv').config();
 
 const express = require('express');
-//const pg = require('pg');
-const { Sequelize } = require('sequelize');
 const bodyParser = require('body-parser');
 const routes = require('./routes/routes');
 
@@ -10,26 +8,6 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
-
-
-// Set up Sequelize connection
-const sequelize = new Sequelize({
-    dialect: 'postgres',
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT || 5432,
-    database: process.env.DB_NAME,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-});
-
-// PostgreSQL configuration
-// const pool = new pg.Pool({
-//     user: process.env.DB_USER,
-//     host: process.env.DB_HOST,
-//     database: process.env.DB_NAME,
-//     password: process.env.DB_PASSWORD,
-//     port: process.env.DB_PORT || 5432
-// });
 
 // Routes
 app.use('/api', routes);
